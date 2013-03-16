cd0381d46caa1d4e44b9c2c62a4f71815885cf26
saladinreborn <saladin.elfajr@gmail.com>
2013-03-16 10:44:21 +0700

    adding entire project

---
+++ application/modules/bea/views/mhs/v-pengumuman.php
@@ -0,0 +1,59 @@
+<div id="content">
+				<div id="content-space">
+					<h2>Penawaran Beasiswa</h2>
+					<div class="scroll">
+						<div class="post">
+							<h3>Beasiswa Supersemar 2013</h3>
+							<p class="post-desc">Baik buruknya proses pelaksanaan Penerimaan Bahasiswa Baru (PMB) menentukan kualitas mahasiswa 
+								yang diterima.  Jangka panjangnya berpengaruh pula terhadap mutu alumni yang dihasilkan. Lebih lebih 
+								lagi, jika mahasiswa yang diterima di setiap prodi tidak sesuai dengan karekter prodi yang bersangkutan 
+								akan berpengaruh buruk terhadap proses pembelajaran selanjutnya.</p>
+								<p class="post-desc">Baik buruknya proses pelaksanaan Penerimaan Bahasiswa Baru (PMB) menentukan kualitas mahasiswa 
+								yang diterima.  Jangka panjangnya berpengaruh pula terhadap mutu alumni yang dihasilkan. Lebih lebih 
+								lagi, jika mahasiswa yang diterima di setiap prodi tidak sesuai dengan karekter prodi yang bersangkutan 
+								akan berpengaruh buruk terhadap proses pembelajaran selanjutnya.</p>
+							<div class="status">Status : </div><div class="status-value">Belum Terdaftar</div>
+							<div class="rfloat">
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Daftar >></a>
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Selengkapnya >></a>
+							</div>
+						</div>
+						<div class="post">
+							<h3>Beasiswa Supersemar 2013</h3>
+							<p class="post-desc">Baik buruknya proses pelaksanaan Penerimaan Bahasiswa Baru (PMB) menentukan kualitas mahasiswa 
+								yang diterima.  Jangka panjangnya berpengaruh pula terhadap mutu alumni yang dihasilkan. Lebih lebih 
+								lagi, jika mahasiswa yang diterima di setiap prodi tidak sesuai dengan karekter prodi yang bersangkutan 
+								akan berpengaruh buruk terhadap proses pembelajaran selanjutnya.</p>
+							<div class="status">Status : </div><div class="status-value">Belum Terdaftar</div>
+							<div class="rfloat">
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Daftar >></a>
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Selengkapnya >></a>
+							</div>
+						</div>
+						<div class="post">
+							<h3>Beasiswa Supersemar 2013</h3>
+							<p class="post-desc">Baik buruknya proses pelaksanaan Penerimaan Bahasiswa Baru (PMB) menentukan kualitas mahasiswa 
+								yang diterima.  Jangka panjangnya berpengaruh pula terhadap mutu alumni yang dihasilkan. Lebih lebih 
+								lagi, jika mahasiswa yang diterima di setiap prodi tidak sesuai dengan karekter prodi yang bersangkutan 
+								akan berpengaruh buruk terhadap proses pembelajaran selanjutnya.</p>
+							<div class="status">Status : </div><div class="status-value">Belum Terdaftar</div>
+							<div class="rfloat">
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Daftar >></a>
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Selengkapnya >></a>
+							</div>
+						</div>
+						<div class="post">
+							<h3>Beasiswa Supersemar 2013</h3>
+							<p class="post-desc">Baik buruknya proses pelaksanaan Penerimaan Bahasiswa Baru (PMB) menentukan kualitas mahasiswa 
+								yang diterima.  Jangka panjangnya berpengaruh pula terhadap mutu alumni yang dihasilkan. Lebih lebih 
+								lagi, jika mahasiswa yang diterima di setiap prodi tidak sesuai dengan karekter prodi yang bersangkutan 
+								akan berpengaruh buruk terhadap proses pembelajaran selanjutnya.</p>
+							<div class="status">Status : </div><div class="status-value">Belum Terdaftar</div>
+							<div class="rfloat">
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Daftar >></a>
+								<a href="#" class="btn-uin btn-inverse btn btn-small" > Selengkapnya >></a>
+							</div>
+						</div>
+					</div>
+				</div>
+	</div>
---
+++ application/modules/skripsi/views/mhs/homeskripsi/contentspace.php
@@ -0,0 +1,22 @@
+<div id="content">	
+	<ul class="navigation">				
+		<a href="<?php echo base_url(); ?>skripsi/getuploadproposal"><li id="tab" class="current">Upload Proposal</li></a>
+		<a href="<?php echo base_url(); ?>skripsi/getbimbingan"><li id="tab">Proses Bimbingan</li></a>
+		<a href="<?php echo base_url(); ?>skripsi/getdfraft"><li id="end-tab">Upload Draft</li></a>
+	</ul>
+			<div id="content-space">
+				<div id="space">
+					<div class="content-value">
+							<font color="#000000" style="font-family:'candara';margin-left:20px;"><h3>Selamat datang</h3></font>
+							<div class="detailcontent">
+								<div class="content-value">
+								<p>Silahkan mengklik menu <b>Upload Proposal</b> untuk mengupload file proposal
+								yang nantinya akan dibaca oleh dosen. Harap menunggu sampai proposal disetujui
+								maka manu <b>Proses Bimbingan </b></p>
+								</div>
+							</div>
+					</div>					
+				</div>					
+			</div>
+		</div>
+	</div>
